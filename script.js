4203911a7bfd4288ff225761ac0a546cbff9ba87
Horia Chindea <168100343+horia-chindea@users.noreply.github.com>
2024-10-07 19:21:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+/**
+ * Calcularea Sumelor:
+ * Definește o funcție numită computeSum care 
+ * primește doi parametri și returnează suma acestora.
+ */
+function computeSum(a, b){
+    // const sum = a + b;
+    // return sum;
+    // sau:
+    return a + b;
+}
+console.log("Suma este:",computeSum(5, 2));
+
+/**
+ * Determinarea Parității:
+ * Scrie o funcție numită isEven care primește un număr 
+ * și returnează true dacă numărul este par și false dacă este impar
+ */
+function isEven(a){
+
+}
+
+/**
+ * Concatenarea Șirurilor:
+ * Creează o funcție numită concatenateStrings care primește 
+ * două șiruri și le concatenează, returnând rezultatul.
+ */
+
+/**
+ * Verificarea Palindromului:
+ * Defineste o funcție numită isPalindrome care primește un șir și
+ *  returnează true dacă este palindrom și false în caz contrar.
+ */
+
+/**
+ * Generarea unui Număr Aleatoriu:
+ * Scrie o funcție numită generateRandomNumber care 
+ * returnează un număr aleatoriu întreg între 1 și 100.
+ */
+
+/**
+ * Calcularea Puterii:
+ * Implementează o funcție numită raiseToPower care primește 
+ * două numere (baza și exponentul) și returnează rezultatul ridicării la putere.
+ */
+
+/**
+ * Calcularea Mediei:
+ * Creează o funcție numită calculateAverage care primește 
+ * un array de numere și returnează media lor.
+ */
+
+/**
+ * Transformarea în Majusculă:
+ * Definește o funcție numită transformToUppercase 
+ * care primește un șir și returnează șirul cu toate 
+ * literele transformate în majusculă.
+ */
+
+/**
+ * Verificarea Numărului Prim:
+ * Scrie o funcție numită isPrimeNumber care primește un număr 
+ * și returnează true dacă este prim și false în caz contrar.
+ */
+
+  
+
